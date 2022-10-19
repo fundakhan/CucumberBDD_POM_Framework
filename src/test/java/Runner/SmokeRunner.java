1c3670492756ebc66ead815b462b3eda8fcf6532
fundakhan <fundaakhan@gmail.com>
2022-10-19 01:47:50 -0400

    update2

--- src/test/java/Runner/SmokeRunner.java
+++ src/test/java/Runner/SmokeRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        tags = "@Regression",
+        tags = "@Smoke",
         features = {"src/test/java/FeatureFile/"},
         glue = {"StepDefinition"},
         plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
